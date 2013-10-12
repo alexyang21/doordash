85d228080c3b96aecb390fd92a7049e41bdec440
Alex Yang <alexyang.personal@gmail.com>
2013-10-11 17:20:38 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Doordash::Application.config.secret_key_base = '37c167679a23ae228fee376cc517ed20898ff4d9082258400479c7eeb66836e39d75e179a69ba3b00023e42ddb0234f77e08414a357ecb9033c8e5e8ac24ea28'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Doordash::Application.config.session_store :cookie_store, key: '_doordash_session'
