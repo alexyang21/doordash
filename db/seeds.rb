4f3825af94045ac1d0221bd3d1123d63d0530a4c
Alex Yang <alexyang.personal@gmail.com>
2013-10-12 19:01:59 -0700

    seed test database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Menu.delete_all
+Menu.create!( restaurant_name:  "California Fish Grill",
+              description:      "Seafood",
+              image_url:        "cfg.jpg")
+Menu.create!( restaurant_name:  "Fukada",
+              description:      "Japanese",
+              image_url:        "fukada.jpg")
+Menu.create!( restaurant_name:  "Mick's Karma Bar",
+              description:      "Burgers",
+              image_url:        "micks.jpg")
