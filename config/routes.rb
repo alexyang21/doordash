7b8cd46ce366901773619cc425bb4cfe889e56f9
Alex Yang <alexyang.personal@gmail.com>
2013-10-14 23:16:47 -0700

    edit routes to change root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Doordash::Application.routes.draw do
   resources :menus
 
+  root 'menus#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -41,7 +43,7 @@ Doordash::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
